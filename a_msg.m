2dc5ffe7fe8db017de34f6be235d76aa3b0b0f2a
frients <1093565669@qq.com>
2022-12-01 18:02:28 +0800

    ygh-v1

--- a_msg.m
+++ a_msg.m
@@ -1,10 +1,4 @@
-%è®¡ç®—ä¸¤ä¸ªRSç çš„ç è·å­ç¨‹åº...ï¼š
-function dis = rscode_dis(code1,code2)
-dis = 0;
-for i = 1:7
- temp = gf_dis(code1(i),code2(i));
- dis = dis+temp;
-end
+%¼ÆËãÁ½¸öRSÂëµÄÂë¾à×Ó³ÌĞò£º 
 function x=a_msg(x)
 x=[0 0 0
  0 0 1
