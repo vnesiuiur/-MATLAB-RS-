43fb3317a199d143b82e8a34ed19a17cb4e1c85f
“Your <3368794629@qq.com>
2022-12-01 09:07:29 +0800

    wanghengV1

--- wangheng_1.m
+++ wangheng_1.m
@@ -1,8 +1,8 @@
 %产生随机的5组信号
 function ret = gf_dis(a,b)
-ret = 0;   
+ret = 0;    
 a_temp = de2bi(gf2num(a),3);
 b_temp = de2bi(gf2num(b),3);
 temp = a_temp+b_temp;
 temp = mod(temp,2);
-ret = sum(temp);
+ret = sum(temp;  
