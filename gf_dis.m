c63bceac3af39ea36e0492c7fc9801ec874ad52d
unknown <1169807747@qq.com>
2022-12-01 21:01:29 +0800

    lizexin V3

--- gf_dis.m
+++ gf_dis.m
@@ -1,10 +1,8 @@
-%计算GF域内两个码字的码距子程序：
-function ret = gf_dis(a,b)
-ret = 0;
-44
-a_temp = de2bi(gf2num(a),3);
-b_temp = de2bi(gf2num(b),3);
-
-temp = a_temp+b_temp;
-temp = mod(temp,2);
-ret = sum(temp);
+%计算GF域内两个码字的码距子程序： 
+function ret = gf_dis(a,b) 
+ret = 0;44 
+a_temp = de2bi(gf2num(a),3); 
+b_temp = de2bi(gf2num(b),3); 
+temp = a_temp+b_temp; 
+temp = mod(temp,2); 
+ret = sum(temp); 
---
+++ rs_rrscode.m
@@ -0,0 +1,4 @@
+%RSÂë±àÂë 
+function new_msg=rs_rrscode(in_msg,all_code_msg,all_msg) 
+new_msg=rs_interpret(in_msg,all_code_msg,all_msg); 
+return; 
